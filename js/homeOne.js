c09339b71930c65b9019ad9104005053416625cf
shanebang29 <69114868+shanebang29@users.noreply.github.com>
2021-03-24 12:38:47 +0700

    final

--- js/homeOne.js
+++ js/homeOne.js
@@ -1,5 +1,22 @@
-var list;
-list = document.querySelectorAll("p");
-for (var i = 0; i < list.length; ++i) {
-   list[i].classList.add('text-muted');
-}
+
+document.addEventListener("DOMContentLoaded",function() {
+   var menu = document.querySelectorAll('.navbar');
+   var menu = menu[0];
+       //Truy xuất div menu
+       var trangthai="duoi840";
+   window.addEventListener("scroll",function(){
+       var x = pageYOffset;
+       if(x > 840){
+           if(trangthai == "duoi840")
+           {
+               trangthai="tren840";
+               menu.classList.add('fixed-navbar');
+           }
+       }
+       else{
+           if(trangthai=="tren840"){
+           menu.classList.remove('fixed-navbar');
+           trangthai="duoi840";}
+       }
+   });
+});
